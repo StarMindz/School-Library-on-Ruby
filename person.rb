56292f4330d09ea641709097a74f8a3c8dfeb1ec
StarMindz <64619873+StarMindz@users.noreply.github.com>
2023-01-03 18:05:41 +0100

    Add nameable class

--- person.rb
+++ person.rb
@@ -1,14 +1,20 @@
+require_relative './nameable.rb'
 # Parent class for all other children classes
-class Person
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   attr_accessors :id, :name, :age
   attr_writer :name, :age
+
+  def correct_name
+    @name
+  end
 
   private
 
