f5cfaec8630f1683989855f1ba05788220a60db0
StarMindz <64619873+StarMindz@users.noreply.github.com>
2023-01-06 18:33:19 +0100

    Create associations between classes

--- student.rb
+++ student.rb
@@ -2,9 +2,16 @@ require_relative './person'
 
 # Inherit all properties and methos of person class. And add playhook function
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
+  end
+
+  def add_class(classroom)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
